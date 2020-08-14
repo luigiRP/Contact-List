ae86b45c96f75ea237e2d377546ad4078eb9f9ff
luigiRP <luigirosales84@gmail.com>
2020-08-14 16:02:34 +0000

    Designing Home

--- src/js/layout.js
+++ src/js/layout.js
@@ -8,7 +8,6 @@ import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -35,7 +34,6 @@ const Layout = () => {
 							<h1>Not found!</h1>
 						</Route>
 					</Switch>
-					<Footer />
 				</ScrollToTop>
 			</BrowserRouter>
 		</div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,104 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container border border-secondary">
+		<div className="row p-3 justify-content-between align-items-center border-bottom border-secondary">
+			<div className="col-6 d-flex">
+				<img src="https://www.luisrp.com/static/media/perfil.440ee5d9.jpg" className="col-4" />
+				<div className="col-6 p-0">
+					<h5 className="mx-3">Luis Rosales</h5>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-map-marker-alt text-secondary mx-3" />
+						<span className="text-secondary">Calle General Pardiñas, 34</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-phone text-secondary mx-3" />
+						<span className="text-secondary">+34 648244118</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="far fa-envelope text-secondary mx-3" />
+						<span className="text-secondary">luigirosales84@gmail.com</span>
+					</div>
+				</div>
+			</div>
+			<div className="col-6 d-flex justify-content-center">
+				<i className="fas fa-pencil-alt mx-5 font" />
+				<i className="fas fa-trash mx-5 text-danger font" />
+			</div>
+		</div>
+		<div className="row p-3 justify-content-between align-items-center border-bottom border-secondary">
+			<div className="col-6 d-flex">
+				<img src="https://www.luisrp.com/static/media/perfil.440ee5d9.jpg" className="col-4" />
+				<div className="col-6 p-0">
+					<h5 className="mx-3">Luis Rosales</h5>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-map-marker-alt text-secondary mx-3" />
+						<span className="text-secondary">Calle General Pardiñas, 34</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-phone text-secondary mx-3" />
+						<span className="text-secondary">+34 648244118</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="far fa-envelope text-secondary mx-3" />
+						<span className="text-secondary">luigirosales84@gmail.com</span>
+					</div>
+				</div>
+			</div>
+			<div className="col-6 d-flex justify-content-center">
+				<i className="fas fa-pencil-alt mx-5 font" />
+				<i className="fas fa-trash mx-5 text-danger font" />
+			</div>
+		</div>
+
+		<div className="row p-3 justify-content-between align-items-center border-bottom border-secondary">
+			<div className="col-6 d-flex">
+				<img src="https://www.luisrp.com/static/media/perfil.440ee5d9.jpg" className="col-4" />
+				<div className="col-6 p-0">
+					<h5 className="mx-3">Luis Rosales</h5>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-map-marker-alt text-secondary mx-3" />
+						<span className="text-secondary">Calle General Pardiñas, 34</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-phone text-secondary mx-3" />
+						<span className="text-secondary">+34 648244118</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="far fa-envelope text-secondary mx-3" />
+						<span className="text-secondary">luigirosales84@gmail.com</span>
+					</div>
+				</div>
+			</div>
+			<div className="col-6 d-flex justify-content-center">
+				<i className="fas fa-pencil-alt mx-5 font" />
+				<i className="fas fa-trash mx-5 text-danger font" />
+			</div>
+		</div>
+
+		<div className="row p-3 justify-content-between align-items-center">
+			<div className="col-6 d-flex">
+				<img src="https://www.luisrp.com/static/media/perfil.440ee5d9.jpg" className="col-4" />
+				<div className="col-6 p-0">
+					<h5 className="mx-3">Luis Rosales</h5>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-map-marker-alt text-secondary mx-3" />
+						<span className="text-secondary">Calle General Pardiñas, 34</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="fas fa-phone text-secondary mx-3" />
+						<span className="text-secondary">+34 648244118</span>
+					</div>
+					<div className="d-flex align-items-baseline">
+						<i className="far fa-envelope text-secondary mx-3" />
+						<span className="text-secondary">luigirosales84@gmail.com</span>
+					</div>
+				</div>
+			</div>
+			<div className="col-6 d-flex justify-content-center">
+				<i className="fas fa-pencil-alt mx-5 font" />
+				<i className="fas fa-trash mx-5 text-danger font" />
+			</div>
+		</div>
 	</div>
 );
