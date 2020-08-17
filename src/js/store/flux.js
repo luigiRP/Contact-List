b6133061f1b18ae9c5d6f51f76d5ab84752ecbf1
luigiRP <luigirosales84@gmail.com>
2020-08-17 07:57:59 +0000

    trying fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,10 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			
+			contacts: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			
 		}
 	};
 };
